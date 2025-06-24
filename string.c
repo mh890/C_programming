33914063632750b46f5df236dcf24aa9ceec500b
Mujdeh Hosein Ali <mujdeh.hosein@gmail.com>
2025-06-23 21:46:38 -0400

    minor changes

--- string.c
+++ string.c
@@ -5,9 +5,8 @@
 STRING is an array of char
 can manipulate strings
     strcpy() -- copy a string over another string
-    strcat() -- apprend a string to another string
+    strcat() -- concatenate a string to another string
     strcmp() -- compare two strings for equality
     strncmp() -- compare the first n charactsers of two strings
     strlen() -- calculate length of string
-
 */
